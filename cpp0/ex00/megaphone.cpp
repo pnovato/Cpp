11d8c2615a3f2701b046c3604c3ea318df706732
pnovato <ptrck.nvt.slv@gmail.com>
2026-02-25 21:37:10 +0000

    cpp ex00

---
+++ cpp0/ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+/*std::string str_toupper(std::string s)
+{
+	std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) {return (std::toupper(c));});
+	return (s);
+}*/
+
+int main(int ac, char **av)
+{
+	int	i;
+	if (ac < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (1);
+	}
+	else
+	{
+		i = 1;
+		while (i < ac)
+		{
+			std::string argument = av[i];
+			std::transform(argument.begin(), argument.end(), argument.begin(), toupper);	
+			//std::cout << str_toupper(argument);
+			std::cout << argument;
+			i++;
+		}
+		std::cout << "\n";
+	}
+	return 0;
+}
