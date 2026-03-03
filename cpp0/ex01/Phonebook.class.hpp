7c6e68364711ceb62f5198c70c592aa8c1e6ea24
pnovato <ptrck.nvt.slv@gmail.com>
2026-03-03 14:54:28 +0000

    testing new implementations

---
+++ cpp0/ex01/Phonebook.class.hpp
@@ -0,0 +1,16 @@
+#ifndef PHONEBOOK_CLASS_HPP
+# define PHONEBOOK_CLASS_HPP
+# include <iostream>
+
+class Phonebook
+{
+public:
+	char	a1;
+	int	a2;
+	float	a3;
+	
+	Phonebook(char p1, int p2, float p3);
+	~Phonebook(void);
+};
+
+#endif
---
+++ cpp0/ex01/main.cpp
@@ -0,0 +1,9 @@
+#include "Phonebook.class.hpp"
+
+int main ()
+{
+	
+	Phonebook instance('a', 42, 4.2f);
+		
+	return (0);
+}
